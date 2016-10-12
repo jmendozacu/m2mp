3feb7d679a65efa113de0dd2ed44a08459bdd185
Michal Kolbusz <michal.kolbusz@polcode.net>
2016-10-12 13:01:53 +0200

    Adding new customer field to registration form

---
+++ app/code/Polcode/GroothandelRegistrationFields/Model/Observer.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Polcode\GroothandelRegistrationFields\Model;
+
+class Observer implements \Magento\Framework\Event\ObserverInterface {
+    
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+//        $request = $observer->getEvent()->getData('request');
+//        var_dump($request->getModuleName());
+//        var_dump($request->getControllerName());
+//        var_dump($request->getActionName());
+//        exit;
+    }
+
+}
---
+++ app/code/Polcode/GroothandelRegistrationFields/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Polcode_GroothandelRegistrationFields',
+    __DIR__
+);
